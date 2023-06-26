9488d933819f12e5d52dc6aeb03f9814ac87ce6c
2HyeonHo <dpqlvkvk@naver.com>
2023-06-26 21:05:28 +0900

    init

---
+++ src/main/java/com/findMyGuide/FindMyGuideApplication.java
@@ -0,0 +1,13 @@
+package com.findMyGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findMyGuide/FindMyGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findMyGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
