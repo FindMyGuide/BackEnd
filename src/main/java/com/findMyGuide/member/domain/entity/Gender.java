30069a7eeee0d224069c37883d4a8883155070dd
newkr <newkr@whatap.io>
2023-06-30 00:05:27 +0900

    feat: openapi 준비 및 의견 제언

--- src/main/java/com/findMyGuide/member/domain/entity/Gender.java
+++ src/main/java/com/findMyGuide/member/domain/entity/Gender.java
@@ -6,6 +6,7 @@ public enum Gender {
     Female ("여");
 
     //front에서 값 출력시 값.krName으로 한글 명칭 가져올 수 있음
+    // krName 보단 kr, en 이나 krGender, enGender 가 좀 더 명확할 것 같아보임. en도 추가할거지?
     private final String krName;
 
     Gender(String krName) {
--- src/main/java/com/findMyGuide/member/domain/entity/Member.java
+++ src/main/java/com/findMyGuide/member/domain/entity/Member.java
@@ -30,6 +30,7 @@ public class Member {
     @Column
     private String nationality;     //Enum으로 관리할 것인가? => 국가가 약 200여개? 정도라 작성하는데 오래걸림
                                     //API로 할것인가? 국가 테이블을 따로 생성해서 가져다 쓸것인가?
+                                    // api or table 찬성, 일단 공공 데이터 포털에 있는 국가 코드 api 예제 코드는 openapi.publica 아래 작성해두었음.
     @Enumerated(EnumType.STRING) //EnumType.ORDINAL : enum의 순서 값, EnumType.STRING : enum의 이름
     @Column
     private Gender gender;
@@ -41,5 +42,5 @@ public class Member {
     private String phoneNumber;
 
     @Column
-    private Boolean certificationYn;
+    private Boolean certificationYn;    //좀 더 명확했으면 좋겠는데, nationalCertificationOfGuideYn 은 너무 길까?
 }
