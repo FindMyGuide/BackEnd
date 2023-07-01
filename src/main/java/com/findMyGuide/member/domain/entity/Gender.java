85c030a163e1907a5ed034bc03f1d746985d87b7
2HyeonHo <dpqlvkvk@naver.com>
2023-07-01 19:16:08 +0900

    refactor: 성별 Enum 영어 필드 추가

--- src/main/java/com/findMyGuide/member/domain/entity/Gender.java
+++ src/main/java/com/findMyGuide/member/domain/entity/Gender.java
@@ -2,20 +2,24 @@ package com.findMyGuide.member.domain.entity;
 
 public enum Gender {
 
-    Male ("남"),
-    Female ("여");
+    MALE ("남", "Male"),
+    FEMALE ("여", "Female");
 
     //front에서 값 출력시 값.krName으로 한글 명칭 가져올 수 있음
     // krName 보단 kr, en 이나 krGender, enGender 가 좀 더 명확할 것 같아보임. en도 추가할거지?
-    // 6.30(금) 이현호 작성
-    // enGender는 기본 값으로 넘어오는거라 .name()으로 가져올 수 있어요. 그래서 따로 안만들어도 될거 같아요
     private final String krGender;
+    private final String enGender;
 
-    Gender(String krGender) {
+    Gender(String krGender, String enGender) {
         this.krGender = krGender;
+        this.enGender = enGender;
     }
 
     public String getKrName() {
         return krGender;
+    }
+
+    public String getEnGender() {
+        return enGender;
     }
 }
