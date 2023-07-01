ca5229473b5de963eddfe49034277c53387eae77
2HyeonHo <dpqlvkvk@naver.com>
2023-07-01 19:18:57 +0900

    feat: 회원 등록 기능 작성

--- src/main/java/com/findMyGuide/member/repository/MemberRepository.java
+++ src/main/java/com/findMyGuide/member/repository/MemberRepository.java
@@ -1,7 +1,14 @@
 package com.findMyGuide.member.repository;
 
 import com.findMyGuide.member.domain.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickName(String nickName);
+
+    Optional<Member> findByPhoneNumber(String phoneNumber);
 }
--- src/main/java/com/findMyGuide/member/service/MemberService.java
+++ src/main/java/com/findMyGuide/member/service/MemberService.java
@@ -1,19 +1,40 @@
 package com.findMyGuide.member.service;
 
-import com.findMyGuide.member.domain.dto.CreateMemberInput;
+import com.findMyGuide.common.DuplicateException;
+import com.findMyGuide.common.ErrorCode;
+import com.findMyGuide.member.domain.dto.CreateMemberRequest;
+import com.findMyGuide.member.domain.dto.CreateMemberResponse;
+import com.findMyGuide.member.domain.entity.Member;
 import com.findMyGuide.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void createMember(CreateMemberInput memberInput) {
+    public CreateMemberResponse createMember(CreateMemberRequest memberRequest) {
+        if(memberRepository.findByEmail(memberRequest.getEmail()).isPresent()) {
+            throw new DuplicateException(memberRequest.getEmail(), ErrorCode.DUPLICATION);
+        }
+
+        if(memberRepository.findByNickName(memberRequest.getNickName()).isPresent()) {
+            throw new DuplicateException(memberRequest.getNickName(), ErrorCode.DUPLICATION);
+        }
+
+        if(memberRepository.findByPhoneNumber(memberRequest.getPhoneNumber()).isPresent()) {
+            throw new DuplicateException(memberRequest.getNickName(), ErrorCode.DUPLICATION);
+        }
+
+        Member member = memberRepository.save(memberRequest.toMember());
+
+        return new CreateMemberResponse(member);
     }
 }
