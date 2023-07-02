5183ab945bdd5e3034f3c48ed9126561c5acde84
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-07-02 21:21:48 +0900

    test : TourProductReview

--- src/test/java/com/find_my_guide/tour_product/service/TourProductServiceTest.java
+++ src/test/java/com/find_my_guide/tour_product/service/TourProductServiceTest.java
@@ -1,30 +1,44 @@
 package com.find_my_guide.tour_product.service;
 
 import com.find_my_guide.common.validation_field.Content;
+import com.find_my_guide.common.validation_field.Title;
 import com.find_my_guide.tour_product.domain.TourProduct;
 import com.find_my_guide.tour_product.repository.TourProductRepository;
 import com.find_my_guide.tour_product_review.domain.TourProductReview;
 import com.find_my_guide.tour_product_review.dto.TourProductReviewRequest;
+import com.find_my_guide.tour_product_review.dto.TourProductReviewResponse;
+import com.find_my_guide.tour_product_review.repository.TourProductReviewRepository;
 import com.find_my_guide.tour_product_review.service.TourProductReviewService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
-public class TourProductServiceTest {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
-    @Autowired
-    private TourProductService tourProductService;
+@ExtendWith(MockitoExtension.class)
+class TourProductServiceTest {
 
-    @Autowired
+    @InjectMocks
+    TourProductService tourProductService;
+
+    @Mock
     TourProductRepository tourProductRepository;
 
-    @Autowired
+    @Mock
+    TourProductReviewRepository tourProductReviewRepository;
+
+    @Mock
     TourProductReviewService tourProductReviewService;
 
     @Test
@@ -32,20 +46,48 @@ public class TourProductServiceTest {
     void register() {
 
 
-        tourProductReviewService.register(1L, new TourProductReviewRequest(2L, "new Review"));
+        // Given
+        TourProduct tourProduct = TourProduct.builder().
+                tourProductId(1L)
+                .title(new Title("hi"))
+                .content(new Content("cotnest"))
+                .build();
 
+        when(tourProductRepository.save(any(TourProduct.class))).thenReturn(tourProduct);
 
+        // When
+        TourProduct savedTourProduct = tourProductRepository.save(tourProduct);
+
+        // Then
+        Assertions.assertNotNull(savedTourProduct.getTourProductId());
+        Assertions.assertEquals(tourProduct.getTitle(), savedTourProduct.getTitle());
     }
 
-    @Test
-    @DisplayName("리뷰 찾기")
-    @Transactional
-    void showReview() {
 
-        Optional<TourProduct> byId = tourProductRepository.findById(1L);
-        List<TourProductReview> tourProductReviews = byId.get().getTourProductReviews();
-        for (TourProductReview tourProductReview : tourProductReviews) {
-            System.out.println("asd   " + tourProductReview.getContent().getContent());
-        }
+    @Test
+    @DisplayName("리뷰 저장")
+    void registerReview() {
+        Long postId = 1L;
+        TourProductReviewRequest reviewRequest = new TourProductReviewRequest(1L,"hi");
+
+
+        TourProduct product = TourProduct.builder().
+                tourProductId(1L)
+                .title(new Title("hi"))
+                .content(new Content("cotnest"))
+                .build();
+
+
+        TourProductReview review = reviewRequest.toTourProductReview();
+        TourProductReviewResponse reviewResponse = new TourProductReviewResponse(review);
+        when(tourProductReviewService.register(eq(postId), any(TourProductReviewRequest.class))).thenReturn(reviewResponse);
+
+        // When
+        TourProductReviewResponse savedReviewResponse = tourProductReviewService.register(postId, reviewRequest);
+
+        // Then
+        Assertions.assertNotNull(savedReviewResponse);
+        Assertions.assertEquals(reviewRequest.getContent(), savedReviewResponse.getContent());
+
     }
 }
