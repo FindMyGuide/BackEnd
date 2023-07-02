fe3969ec93c270c38f6def587fdea611967b72a3
2HyeonHo <dpqlvkvk@naver.com>
2023-07-02 18:45:46 +0900

    refactor: DB 컬럼 매핑을 위한 name 지정

--- src/main/java/com/findMyGuide/member/domain/entity/Member.java
+++ src/main/java/com/findMyGuide/member/domain/entity/Member.java
@@ -15,32 +15,33 @@ import lombok.NoArgsConstructor;
 public class Member {
 
     @Id
+    @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "MEM_SEQ")
     private Long idx;
 
-    @Column
+    @Column(nullable = false)
     private String email;
 
-    @Column
+    @Column(nullable = false)
     private String password;
 
-    @Column
-    private String nickName;
+    @Column(nullable = false)
+    private String nickname;
 
-    @Column
+    @Column(nullable = false)
     private String nationality;     //Enum으로 관리할 것인가? => 국가가 약 200여개? 정도라 작성하는데 오래걸림
                                     //API로 할것인가? 국가 테이블을 따로 생성해서 가져다 쓸것인가?
                                     // api or table 찬성, 일단 공공 데이터 포털에 있는 국가 코드 api 예제 코드는 openapi.publica 아래 작성해두었음.
     @Enumerated(EnumType.STRING) //EnumType.ORDINAL : enum의 순서 값, EnumType.STRING : enum의 이름
-    @Column
+    @Column(nullable = false)
     private Gender gender;
 
-    @Column
+    @Column(nullable = false)
     private Integer age;
 
-    @Column
+    @Column(name = "phone_number")
     private String phoneNumber;
 
-    @Column
-    private Boolean nationalCertificationOfGuideYn;    //좀 더 명확했으면 좋겠는데, nationalCertificationOfGuideYn 은 너무 길까?   //좋죠
+    @Column(name = "national_certification_of_guide_yn", nullable = false)
+    private Boolean nationalCertificationOfGuideYn;
 }
