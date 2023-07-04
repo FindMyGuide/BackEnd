c92c9ffa958c686b9d1be4cee62fea73bd5af476
2HyeonHo <dpqlvkvk@naver.com>
2023-07-04 23:03:16 +0900

    test: 회원 등록 controller 테스트 작성

---
+++ src/test/java/com/findMyGuide/member/controller/MemberRestControllerTest.java
@@ -0,0 +1,72 @@
+package com.findMyGuide.member.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.findMyGuide.member.domain.dto.CreateMemberRequest;
+import com.findMyGuide.member.domain.dto.CreateMemberResponse;
+import com.findMyGuide.member.domain.entity.Member;
+import com.findMyGuide.member.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MemberRestController.class)
+class MemberRestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    private CreateMemberRequest memberRequest;
+
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        memberRequest = new CreateMemberRequest(
+            "abc@naver.com",
+            "@qwer12345",
+            "test",
+            "KR",
+            "MALE",
+            25,
+            "010-1234-5678",
+            false
+        );
+        member = memberRequest.toMember();
+    }
+
+    @Test
+    @DisplayName("회원 등록")
+    @WithMockUser
+    void register() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(memberRequest);
+
+        given(memberService.createMember(memberRequest))
+            .willReturn(new CreateMemberResponse(member));
+
+        mvc.perform(
+            post("/find-my-guide/sign-up")
+                .with(csrf())
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isCreated());
+//            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//            .andExpect(jsonPath("$.email", equalTo("abc@naver.com")));
+    }
+}
