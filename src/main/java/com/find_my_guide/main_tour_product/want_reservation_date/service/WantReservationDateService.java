605a7f0eeeec2cf0e50d8ba6a56414e979bb6dd6
chanwoong114 <cksdnd10001@gmail.com>
2023-07-20 00:50:07 +0900

    WantTourProduct

--- src/main/java/com/find_my_guide/main_tour_product/want_reservation_date/service/WantReservationDateService.java
+++ src/main/java/com/find_my_guide/main_tour_product/want_reservation_date/service/WantReservationDateService.java
@@ -31,13 +31,23 @@ public class WantReservationDateService {
         return new WantReservationDateResponse(wantReservationDateRepository.save(wantReservationDate));
     }
 
+    @Transactional
+    public void deleteDate(Long wantTourProductId, Long wantReservationDateId) {
+
+    }
+
     private void isSameDate(WantTourProduct wantTourProduct, WantReservationDate wantReservationDate) {
         if (wantTourProduct.getWantReservationDates().contains(wantReservationDate.getDate())) {
             throw new IllegalArgumentException("같은 날짜가 존재함");
         }
     }
 
+
     private WantTourProduct findWantTourProduct(Long id) {
         return wantTourProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 글 입니다."));
+    }
+
+    private WantReservationDate findWantReservation(Long id) {
+        return wantReservationDateRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택하지 않은 날입니다."));
     }
 }
