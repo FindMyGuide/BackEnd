5c902324aa97a2be1c56e1803051c5acccf7bd0e
chanwoong114 <cksdnd10001@gmail.com>
2023-09-01 17:57:29 +0900

    web config 수정

---
+++ src/main/java/com/find_my_guide/main_member/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.find_my_guide.main_member.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public static final String ALLOWED_METHOD_NAMES = "GET, HEAD, POST, PUT, DELETE";
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("https://find-my-guide.site", "http://13.58.216.103:3000",
+                        "http://localhost:3000")
+                .allowedMethods((ALLOWED_METHOD_NAMES.split(",")));
+    }
+}
