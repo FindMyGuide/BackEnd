8b0fa2ea2786d1c7eb072ee9b1ff6957c068be0d
Jang Tae Won <xodnjs1837@gmail.com>
2023-10-12 17:28:42 +0900

    fix : cors 에러

--- src/main/java/com/find_my_guide/main_member/config/WebMvcConfig.java
+++ src/main/java/com/find_my_guide/main_member/config/WebMvcConfig.java
@@ -8,26 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    public static final String ALLOWED_METHOD_NAMES = "GET, HEAD, POST, PUT, DELETE, PATCH";
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("https://find-my-guide.site", "http://13.58.216.103:3000",
-                        "http://localhost:3000")
-                .allowedMethods((ALLOWED_METHOD_NAMES.split(",")));
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
-
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                        .allowedOrigins("*")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
 }
