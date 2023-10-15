f3e303b964e90ad1559bd552c739ee7d07c9f371
chanwoong114 <cksdnd10001@gmail.com>
2023-10-15 19:09:43 +0900

    원해요 삭제 로직 수정

---
+++ src/main/java/com/find_my_guide/main_tour_product/want_tour_product/dto/WantTourProductIdRequest.java
@@ -0,0 +1,12 @@
+package com.find_my_guide.main_tour_product.want_tour_product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WantTourProductIdRequest {
+    private Long wantTourProductId;
+}
