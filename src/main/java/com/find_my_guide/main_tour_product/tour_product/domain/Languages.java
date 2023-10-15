d50cf9f5859556a0373481a997ee42140581736a
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-10-15 16:29:05 +0900

    feat : Languages 한글로 바꿈

--- src/main/java/com/find_my_guide/main_tour_product/tour_product/domain/Languages.java
+++ src/main/java/com/find_my_guide/main_tour_product/tour_product/domain/Languages.java
@@ -2,14 +2,34 @@ package com.find_my_guide.main_tour_product.tour_product.domain;
 
 public enum Languages {
 
-    한국어,
-    영어,
-    스페인어,
-    중국어,
-    일본어,
-    프랑스어,
-    독일어,
-    러시아어,
-    이탈리아어,
-    포르투갈어;
+    KOREAN("한국어"),
+    ENGLISH("영어"),
+    SPANISH("스페인어"),
+    CHINESE("중국어"),
+    JAPANESE("일본어"),
+    FRENCH("프랑스어"),
+    GERMAN("독일어"),
+    RUSSIAN("러시아어"),
+    ITALIAN("이탈리아어"),
+    PORTUGUESE("포르투갈어");
+
+    private final String koreanName;
+
+    Languages(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
+
+    public static Languages fromString(String languageName) {
+        for (Languages language : Languages.values()) {
+            if (language.getKoreanName().equalsIgnoreCase(languageName)) {
+                return language;
+            }
+        }
+        throw new IllegalArgumentException("No language found for: " + languageName);
+    }
 }
+
