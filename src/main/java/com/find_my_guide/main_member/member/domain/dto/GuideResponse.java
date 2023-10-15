30930952666351e51719b175e7837fe9dfa99b09
xodnjs8287 <xodnjs8287@naver.com>
2023-10-15 18:36:54 +0900

    create : 가이드 response null check

--- src/main/java/com/find_my_guide/main_member/member/domain/dto/GuideResponse.java
+++ src/main/java/com/find_my_guide/main_member/member/domain/dto/GuideResponse.java
@@ -28,17 +28,19 @@ public class GuideResponse {
 
     public GuideResponse(Member member) {
         this.guideId = member.getIdx();
-        this.guideName = member.getName();
-        this.guideEmail = member.getEmail();
-        this.gender = member.getGender().getKrName();
-        this.hasGuideCertification = member.getNationalCertificationOfGuideYn();
-        this.tourProductTitles = member.getTourHistoriesAsGuide().stream()
+        this.guideName = member.getName() != null ? member.getName() : "";
+        this.guideEmail = member.getEmail() != null ? member.getEmail() : "";
+        this.gender = (member.getGender() != null && member.getGender().getKrName() != null) ? member.getGender().getKrName() : "";
+        this.hasGuideCertification = member.getNationalCertificationOfGuideYn() != null ? member.getNationalCertificationOfGuideYn() : false; // 가정: getNationalCertificationOfGuideYn()의 반환 타입이 Boolean임
+        this.tourProductTitles = (member.getTourHistoriesAsGuide() != null) ? member.getTourHistoriesAsGuide().stream()
+                .filter(it -> it.getTourProduct() != null && it.getTourProduct().getTitle() != null)
                 .map(it -> it.getTourProduct().getTitle())
-                .collect(Collectors.toList());
-        this.guideExperience = (member.getGuideExperience() != null) ? member.getGuideExperience() : 0;
-        this.languages = (member.getLanguages() != null) ? member.getLanguages() : new ArrayList<>();
-        this.guideIntro = (member.getGuideIntro() != null) ? member.getGuideIntro() : "";
-        this.profilePicture = (member.getProfilePicture() != null) ? member.getProfilePicture() : "";
-        this.guideCertification = member.getGuideCertification();
+                .collect(Collectors.toList()) : new ArrayList<>();
+        this.guideExperience = member.getGuideExperience() != null ? member.getGuideExperience() : 0;
+        this.languages = member.getLanguages() != null ? member.getLanguages() : new ArrayList<>();
+        this.guideIntro = member.getGuideIntro() != null ? member.getGuideIntro() : "";
+        this.profilePicture = member.getProfilePicture() != null ? member.getProfilePicture() : "";
+        this.guideCertification = member.getGuideCertification() != null ? member.getGuideCertification() : ""; // 가정: getGuideCertification()의 반환 타입이 String임
     }
+
 }
