7118adeeacb6cb6aa58c9c8c06c9f79d1028b73e
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-10-15 16:15:09 +0900

    feat : Languages 한글로 바꿈

--- src/main/java/com/find_my_guide/main_tour_product/tour_product/domain/Languages.java
+++ src/main/java/com/find_my_guide/main_tour_product/tour_product/domain/Languages.java
@@ -2,14 +2,14 @@ package com.find_my_guide.main_tour_product.tour_product.domain;
 
 public enum Languages {
 
-    KOREAN,
-    ENGLISH,
-    SPANISH,
-    CHINESE,
-    JAPANESE,
-    FRENCH,
-    GERMAN,
-    RUSSIAN,
-    ITALIAN,
-    PORTUGUESE;
+    한국어,
+    영어,
+    스페인어,
+    중국어,
+    일본어,
+    프랑스어,
+    독일어,
+    러시아어,
+    이탈리아어,
+    포르투갈어;
 }
