108afa2c20a2ee53d049127cc841607e37721fbd
xodnjs8287 <xodnjs8287@naver.com>
2023-10-20 00:51:01 +0900

    fix : 언어 수정

---
+++ src/main/java/com/find_my_guide/main_member/member/domain/dto/GuideSearchResponse.java
@@ -0,0 +1,38 @@
+package com.find_my_guide.main_member.member.domain.dto;
+
+import com.find_my_guide.main_member.member.domain.entity.Member;
+import com.find_my_guide.main_tour_product.tour_product.domain.Languages;
+import com.find_my_guide.main_tour_product.tour_product.dto.TourProductResponse;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class GuideSearchResponse {
+
+    private Long guideId;
+    private String guideName;
+    private String guideEmail;
+    private String gender;
+    private boolean hasGuideCertification;
+    private int guideExperience;
+
+    private String guideIntro;
+    private String guideCertification;
+    private String profilePicture;
+    private List<TourProductInformationResponse> tourProductInformation;
+
+    public GuideSearchResponse(Member member) {
+        this.guideId = member.getIdx();
+        this.guideName = member.getName() != null ? member.getName() : "";
+        this.guideEmail = member.getEmail() != null ? member.getEmail() : "";
+        this.gender = (member.getGender() != null && member.getGender().getKrName() != null) ? member.getGender().getKrName() : "";
+        this.hasGuideCertification = member.getNationalCertificationOfGuideYn() != null ? member.getNationalCertificationOfGuideYn() : false; // 가정: getNationalCertificationOfGuideYn()의 반환 타입이 Boolean임
+        this.guideExperience = member.getGuideExperience() != null ? member.getGuideExperience() : 0;
+        this.guideIntro = member.getGuideIntro() != null ? member.getGuideIntro() : "";
+        this.profilePicture = member.getProfilePicture() != null ? member.getProfilePicture() : "";
+        this.guideCertification = member.getGuideCertification() != null ? member.getGuideCertification() : ""; // 가정: getGuideCertification()의 반환 타입이 String임
+    }
+
+}
